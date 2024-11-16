2e6b319289cbe177564960dbc80e392723b97db4
tapuchandramojumder <mojumdertapu@gmail.com>
2024-11-16 14:46:42 +0600

    page3 text  Horizontal scrolling done

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+// Image animation start top 0 and finished top -100% where page was pin, in this case verical scrolling happend 
 gsap.to("#page2 img",{
     width:"100%",
     scrollTrigger:{
         trigger:"#page2",
+        scroller:"body",
         start:"top 0%",
         end:"top -100%",
         markers:true,
         scrub:2,
+        pin:true
+    }
+})
+
+// Text animaation start top 0 and finished top 100% where page was pin, in this case Horizontal scrolling happened
+
+gsap.to("#page3 h1",{
+    transform:"translateX(-90%)",
+    scrollTrigger:{
+        trigger:"#page3",
+        scroller:"body",
+        start:"top 0%",
+        end:"top -100%",
+        markers:true,
+        scrub:3,
         pin:true
     }
 })
